97e0c7549efbe01fc2e310119d861cff8ed39636
Colin Tseng <klt.aus@yahoo.com>
2021-03-28 20:55:46 +1100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const inputMessage = document.getElementById("new-task-description");
+  const submitButton = document.querySelector("input[type=submit]");
+  const lists = document.getElementById(list);
+
+  inputMessage.value;
+
+  submitButton.addEventListener('click', function(event) {
+
+    let listItem = document.createElement("li");
+    listItem.innerHTML = inputMessage.value;
+    document.getElementById("tasks").appendChild(listItem);
+    event.preventDefault();
+
+  })
+
 });
